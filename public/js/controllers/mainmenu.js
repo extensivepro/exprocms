04b1264bc7d5aae32ecd7bffb4fa127c632f9ca1
Zhanwen_Xu <leonardoipod@gmail.com>
2013-12-27 18:12:37 +0800

    ssue2&3 completed with conflict cleaned. resulting functionalities: 网站管理 post管理：成列，增删改查，Markdown及时渲染，图片上传功能

--- public/js/controllers/mainmenu.js
+++ public/js/controllers/mainmenu.js
@@ -1,18 +1,13 @@
-function MainMenuController($scope){
-	$scope.views = [ 
-	/*	  {name:"会员管理", icon:"icon-user", path:"views/member/index.html", submenus:[
-					{name:"储值账单", icon:"icon-list", path:"views/member/balanceIndex.html"}
-				,	{name:"会员充值", icon:"icon-money", path:"views/member/recharge.html"}
-				,	{name:"积分清单", icon:"icon-list-alt", path:"views/member/pointLogIndex.html"}
-				,	{name:"积分累积", icon:"icon-plus", path:"views/member/accumulate.html"}
-			]}
-			, {name:"门店管理", icon:"icon-folder-close-alt", path:"views/shop/index.html", submenus: [
-					{name:"门店员工", icon:"icon-hdd", path:"views/staff/index.html"}
-			]}
-			, */{name:"系统用户", icon:"icon-user-md", path:"views/user/index.html"},
-			 {name:"文章编辑", icon:"icon-user-md", path:"views/article/index.html"}
+function MainMenuController($scope, $routeParams, $location){
+	$scope.views = [
+        {name:"系统用户", icon:"icon-user-md", path:"views/user/index.html"},
+        {name:"网站管理", icon:"icon-hdd", path:"views/site_manage/site.html"},
+	    {name:"文章管理", icon:"icon-list", path:"views/post/list.html"},
+        {name:"新建文章", icon:"icon-plus", path:"views/post/create.html"},
+        {name:"修改文章", icon:"icon-money", path:"views/post/edit.html"}
 	]
-	$scope.currentView = $scope.views[0];
+
+	$scope.currentView = $scope.views[$location.search().view | 0];
 	
 	$scope.selectView = function(view) {
 		$scope.currentView = view
